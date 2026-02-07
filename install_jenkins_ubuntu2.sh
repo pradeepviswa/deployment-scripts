defb1c9c9da434dc7d78396beb4ce7ef421316fe
Pradeep Viswanathan <74972223+pradeepviswa@users.noreply.github.com>
2026-02-07 23:01:17 +0530

    Update install_jenkins_ubuntu2.sh

--- install_jenkins_ubuntu2.sh
+++ install_jenkins_ubuntu2.sh
@@ -3,21 +3,23 @@ sudo apt update
 sudo apt upgrade -y
 
 # 2️⃣ Install dependencies
-sudo apt install -y openjdk-17-jdk
-
+sudo apt install fontconfig openjdk-21-jre
+java -version
 
 # 3️⃣ Add Jenkins official repository key (NEW method)
 curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
 # 4️⃣ Add Jenkins repository
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-https://pkg.jenkins.io/debian-stable binary/" | sudo tee \
-/etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 # 5️⃣ Update apt and install Jenkins
 sudo apt update
-sudo apt install -y jenkins
+sudo apt install jenkins
 
 # 6️⃣ Start & enable Jenkins
 sudo systemctl enable jenkins
